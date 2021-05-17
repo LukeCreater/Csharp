4225f6c8b05d84098152562c65d4c6c3affa7d90
Lucas Corrêa <63137745+lukinia@users.noreply.github.com>
2021-05-17 13:56:25 -0300

    Object e ToString()

---
+++ Exercicios com Classes 02/Produto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque ()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome}, ${Preco}, {Quantidade} restantes e {ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)} valor acumulado."; 
+        }
+    }
+}
---
+++ Exercicios com Classes 02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.WriteLine("Nome do produto:");
+            p.Nome = Console.ReadLine();
+            Console.WriteLine("Preço do produto:");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Quantidade do produto:");
+            p.Quantidade = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Dados do produto: {p}");
+
+        }
+    }
+}
