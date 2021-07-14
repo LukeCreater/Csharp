b20ba4ead83fb41c697aee8a9aff76dea356470f
Lucas Corrêa <63137745+lukinia@users.noreply.github.com>
2021-07-14 19:12:29 -0300

    Exercícios com Get e Set (Auto Properties)
    Não existe uso de Construtor. Apenas atribuição direta para as
    variáveis privates utilizando get e set

---
+++ Get e Set/Produto.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace Exercicio_Get_e_Set
+{
+    class Produto
+    {
+        private string nome;
+        private double quantidade;
+        private double preco;
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public double Quantidade
+        {
+            get { return quantidade; }
+            set { quantidade = value; }
+        }
+
+        public double Preco
+        {
+            get { return preco; }
+            set { preco = value; }
+        }
+
+        public override string ToString()
+        {
+            return "Nome do Produto: " + nome + ", Quantidade: " + quantidade + ", Preço: " + preco.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Get e Set/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercicio_Get_e_Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+            Console.Write("Nome do Produto: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Quantidade do Produto: ");
+            p.Quantidade = double.Parse(Console.ReadLine());
+            Console.Write("Preço do Produto: ");
+            p.Preco = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(p.ToString());
+            
+        }
+    }
+}
