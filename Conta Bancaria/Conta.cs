84b8c8f06d8c6e0d7964f5e154bf00ecba36538a
Lucas Corrêa <63137745+lukinia@users.noreply.github.com>
2021-07-14 18:40:51 -0300

    Exercício com Conta Bancária

---
+++ Conta Bancaria/Conta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Course
+{
+    class Conta
+    {
+        public string Titular { get; set; }
+        public int Numero { get; private set; }
+        public double Saldo { get; private set; }
+
+        public Conta(string titular, int numero)
+        {
+            Titular = titular;
+            Numero = numero;
+        }
+        public Conta(string titular, int numero, double deposito) : this(titular, numero)
+        {
+            Saldo += Depositar(deposito);
+        }
+
+        private double Depositar(double valor)
+        {
+            valor -= 5.00; // Taxa de $ 5.00
+            return valor;
+        }
+
+        public override string ToString()
+        {
+            return $"Titular: {Titular}, Numero: {Numero}, Saldo: {Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Conta Bancaria/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta c;
+            Console.Write("Digite o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Digite o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial? (s/n): ");
+            char depositoInicial = char.Parse(Console.ReadLine());
+            if (depositoInicial == 's' || depositoInicial == 'S')
+            {
+                Console.Write("Digite o valor para depósito inicial: ");
+                double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                c = new Conta(titular, numero, deposito);
+                Console.WriteLine(c.ToString());
+            }
+            else if (depositoInicial == 'n' || depositoInicial == 'N')
+            {
+                c = new Conta(titular, numero);
+                Console.WriteLine(c.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Você não informou se terá depósito inicial ou não.");
+            }
+
+            Console.Clear();
+        }
+    }
+}
